b15dfb0dc37aeeae4c91877688dbea5bcb20f1b9
chomp105 <54012005+chomp105@users.noreply.github.com>
2022-02-25 07:18:00 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//import { Circle, Line, CircleCircleCollision, CircleLineCollision, Chomp } from "./engine.js"
-
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -7,14 +5,40 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const engine = require('chompengine');
+const chomp = new engine.Chomp(0, 0, 1000, 1000);
+let orders = [];
 
+app.use(express.static(__dirname+'/public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-let chomp = new engine.Chomp(0, 0, 400, 400);
-
-let orders = [];
+io.on('connection', (socket) => {
+	chomp.circles.push(new engine.Circle(Math.random() * 400, Math.random() * 400, 50, socket.id, 0));
+	orders.push({id: socket.id, order: chomp.circles.length - 1});
+	socket.emit('update', {circles: chomp.circles, lines: chomp.lines, wx1: chomp.wx1, wy1: chomp.wy1, wx2: chomp.wx2, wy2: chomp.wy2, orders: orders });
+	socket.emit('start');
+	socket.on('action', (action) => {
+		for (let i = 0; i < orders.length; i++) {
+			if (orders[i].id == socket.id) {
+				chomp.circles[orders[i].order].x = action.x;
+				chomp.circles[orders[i].order].y = action.y;
+				chomp.circles[orders[i].order].angle = action.angle;
+			}
+		}
+	});
+	socket.on('disconnect', () => {
+		for (let i = 0; i < orders.length; i++) {
+			if (orders[i].id == socket.id) {
+				chomp.circles.splice(orders[i].order, 1);
+				for (let j = i; j < orders.length; j++) {
+					orders[j].order--;
+				}
+				orders.splice(i, 1);
+			}
+		}
+	});
+});
 
 setInterval(() => {
 	chomp.wallCollisions();
@@ -22,36 +46,7 @@ setInterval(() => {
 	chomp.resolveCircleCircleCollisions();
 	chomp.circleCircleCollisions = [];
 	io.sockets.emit('update', {circles: chomp.circles, lines: chomp.lines, wx1: chomp.wx1, wy1: chomp.wy1, wx2: chomp.wx2, wy2: chomp.wy2, orders: orders });
-}, 1000/120);
-
-io.on('connection', (socket) => {
-	chomp.circles.push(new engine.Circle(Math.random() * 400, Math.random() * 400, 50, "dynamic"));
-	orders.push({id: socket.id, order: chomp.circles.length - 1});
-	socket.emit('update', {circles: chomp.circles, lines: chomp.lines, wx1: chomp.wx1, wy1: chomp.wy1, wx2: chomp.wx2, wy2: chomp.wy2, orders: orders });
-	socket.emit('start');
-	socket.on('action', (action) => {
-		for (let i = 0; i < orders.length; i++) {
-			if (orders[i].id == action.id) {
-				chomp.circles[i].x = action.x;
-				chomp.circles[i].y = action.y;
-			}
-		}
-	});
-	socket.on('disconnect', () => {
-		let count;
-		for (let i = 0; i < orders.length; i++) {
-			if (orders[i].id == socket.id) {
-				count = i;
-			}
-		}
-		console.log(socket.id + " joined!");
-		for (let i = count + 1; i < orders.length; i++) {
-			orders[i].order--;
-		}
-		orders.splice(count, 1);
-		chomp.circles.splice(count, 1);
-	});
-});
+}, 0);
 
 server.listen(3000, () => {
 	console.log('listening on *:3000');
